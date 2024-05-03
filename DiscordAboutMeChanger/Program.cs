ea97d9d420ea13de655b2ff38bfa11b72a88c40c
lyze <66133638+HakuSystems@users.noreply.github.com>
2024-05-03 21:03:22 +0200

    Add DiscordAboutMeChanger application
    This commit introduces a new application, DiscordAboutMeChanger, that prompts the user for an account balance and generates an updated message for the "About Me" section. This application also includes necessary Visual Studio solution and project files, as well as a new .gitignore file for .NET projects.

---
+++ DiscordAboutMeChanger/Program.cs
@@ -0,0 +1,20 @@
+﻿using TextCopy;
+
+namespace DiscordAboutMeChanger;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var userInput = "";
+        var unixTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+        Console.WriteLine("Enter your account balance: ");
+        userInput = Console.ReadLine();
+        var aboutMeString = $":moneybag: Account Balance (as of **<t:{unixTimestamp}:R>**): **{userInput}** \n`This value gets updated every 1-3 days`";
+        Console.WriteLine("Your new about me string is: ");
+        Console.WriteLine(aboutMeString);
+        ClipboardService.SetText(aboutMeString);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
