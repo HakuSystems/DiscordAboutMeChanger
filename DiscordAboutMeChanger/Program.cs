326199a11c6347905908a566ce37d74269faa37f
lyze <66133638+HakuSystems@users.noreply.github.com>
2024-05-14 17:00:02 +0200

    Update aboutMeString and remove exit prompt
    An additional sentence has been added to the "aboutMeString" variable, enhancing the user's profile. The program's exit prompt, which required user input, has been removed to streamline the program's runtime.

--- DiscordAboutMeChanger/Program.cs
+++ DiscordAboutMeChanger/Program.cs
@@ -10,11 +10,9 @@ class Program
         var unixTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
         Console.WriteLine("Enter your account balance: ");
         userInput = Console.ReadLine();
-        var aboutMeString = $":moneybag: Account Balance (as of **<t:{unixTimestamp}:R>**): **{userInput}** \n`This value gets updated every 1-3 days`";
+        var aboutMeString = $"**Jack of all trades, master of none, but oftentimes better than a master of one.** \n:moneybag: Account Balance (as of **<t:{unixTimestamp}:R>**): **{userInput}** \n`This value gets updated every 1-3 days`";
         Console.WriteLine("Your new about me string is: ");
         Console.WriteLine(aboutMeString);
         ClipboardService.SetText(aboutMeString);
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
     }
 }
